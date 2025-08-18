405f68a34973c9c2abe75a225e05cd705d758ee3
Aurélien Devaux <github@aureldvx.net>
2025-08-18 11:11:32 +0200

    ref: ical generator

--- app/Http/Controllers/Public/ICalController.php
+++ app/Http/Controllers/Public/ICalController.php
@@ -3,26 +3,24 @@
 namespace App\Http\Controllers\Public;
 
 use App\Http\Controllers\Controller;
-use App\Models\User;
+use App\Models\HumanResource\Person;
 use App\Services\ICalGenerator;
 
 class ICalController extends Controller
 {
-    public function __construct(private readonly ICalGenerator $iCalGenerator)
-    {
-    }
+    public function __construct(private readonly ICalGenerator $iCalGenerator) {}
 
-    public function streamUserCalendar(User $user)
+    public function streamPersonCalendar(Person $person)
     {
-        $calendar = $this->iCalGenerator->generate($user);
+        $calendar = $this->iCalGenerator->generate($person);
 
         return response($calendar->get())
             ->header('Content-Type', 'text/calendar; charset=utf-8');
     }
 
-    public function downloadUserCalendar(User $user)
+    public function downloadPersonCalendar(Person $person)
     {
-        $calendar = $this->iCalGenerator->generate($user);
+        $calendar = $this->iCalGenerator->generate($person);
 
         return response($calendar->get(), 200, [
             'Content-Type' => 'text/calendar; charset=utf-8',
