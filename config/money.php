bc932e8b60ece13ad2ee986341a235e07d66259a
Aurélien Devaux <github@aureldvx.net>
2025-08-18 11:09:59 +0200

    chore: moneyphp installation

---
+++ config/money.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    /*
+     |--------------------------------------------------------------------------
+     | Laravel money
+     |--------------------------------------------------------------------------
+     */
+    'locale' => 'fr_FR',
+    'defaultCurrency' => 'EUR',
+    'defaultFormatter' => null,
+    'currencies' => [
+        'iso' => ['EUR'],
+    ],
+];
