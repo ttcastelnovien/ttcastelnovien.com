5dcc24719b1f1717efa7efecb99a8bec9c24607a
Aurélien Devaux <github@aureldvx.net>
2025-08-18 11:09:45 +0200

    chore: filesystems

--- config/filesystems.php
+++ config/filesystems.php
@@ -8,38 +8,40 @@ return [
 
         'local' => [
             'driver' => 'local',
-            'root' => storage_path('app/private'),
+            'root' => storage_path('app/private/files'),
+            'url' => env('APP_URL').'/storage/files',
             'serve' => true,
             'throw' => false,
             'report' => false,
         ],
 
-        'public' => [
+        'clubs' => [
             'driver' => 'local',
-            'root' => storage_path('app/public'),
-            'url' => env('APP_URL').'/storage',
-            'visibility' => 'public',
+            'root' => storage_path('app/private/clubs'),
+            'url' => env('APP_URL').'/storage/clubs',
+            'serve' => true,
             'throw' => false,
             'report' => false,
         ],
 
-        's3' => [
-            'driver' => 's3',
-            'key' => env('AWS_ACCESS_KEY_ID'),
-            'secret' => env('AWS_SECRET_ACCESS_KEY'),
-            'region' => env('AWS_DEFAULT_REGION'),
-            'bucket' => env('AWS_BUCKET'),
-            'url' => env('AWS_URL'),
-            'endpoint' => env('AWS_ENDPOINT'),
-            'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
+        'invoices' => [
+            'driver' => 'local',
+            'root' => storage_path('app/private/invoices'),
+            'url' => env('APP_URL').'/storage/invoices',
+            'serve' => true,
             'throw' => false,
             'report' => false,
         ],
 
-    ],
+        'certificates' => [
+            'driver' => 'local',
+            'root' => storage_path('app/private/certificates'),
+            'url' => env('APP_URL').'/storage/certificates',
+            'serve' => true,
+            'throw' => false,
+            'report' => false,
+        ],
 
-    'links' => [
-        public_path('storage') => storage_path('app/public'),
     ],
 
 ];
