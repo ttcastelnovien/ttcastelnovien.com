950aef79344190c8081dab828b29f45df7a724bc
Aurélien Devaux <github@aureldvx.net>
2025-08-20 11:30:31 +0200

    ref: remove global HandleInertiaRequests middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,7 +1,6 @@
 <?php
 
 use App\Http\Middleware\HandleAppearance;
-use App\Http\Middleware\HandleInertiaRequests;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -18,7 +17,6 @@ return Application::configure(basePath: dirname(__DIR__))
 
         $middleware->web(append: [
             HandleAppearance::class,
-            HandleInertiaRequests::class,
             AddLinkHeadersForPreloadedAssets::class,
         ]);
     })
