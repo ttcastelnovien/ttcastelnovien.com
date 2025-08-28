e2d1cd167dd95b1cd5613462ed03beea4700af3f
Aurélien Devaux <github@aureldvx.net>
2025-08-28 15:32:59 +0200

    fix(docker): deploy

--- frankenphp/entrypoint.sh
+++ frankenphp/entrypoint.sh
@@ -9,7 +9,7 @@ cleanup() {
     echo "container stopping..."
     kill -TERM "$OCTANE_PID" 2>/dev/null
     kill -TERM "$SSR_PID" 2>/dev/null
-    wait
+    wait $OCTANE_PID $SSR_PID
     exit 0
 }
 trap cleanup TERM INT
@@ -19,3 +19,5 @@ php artisan inertia:start-ssr --no-ansi & SSR_PID=$!
 
 echo "starting frankenphp..."
 php artisan octane:frankenphp --no-ansi & OCTANE_PID=$!
+
+wait $OCTANE_PID $SSR_PID
