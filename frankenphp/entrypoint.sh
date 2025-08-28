f98bddf79c18574745cd6c2bb097a0f48625a74a
Aurélien Devaux <github@aureldvx.net>
2025-08-28 14:56:13 +0200

    fix(docker): deploy

--- frankenphp/entrypoint.sh
+++ frankenphp/entrypoint.sh
@@ -1,11 +1,21 @@
 #!/bin/sh
 set -e
 
-# Optimize Laravel
-php artisan optimize;
+echo "laravel preparation..."
 
-# Start Inertia in SSR mode
-php artisan inertia:start-ssr
+php artisan optimize --no-ansi
 
-# Start FrankenPHP (Octane)
-php artisan octane:frankenphp
+cleanup() {
+    echo "container stopping..."
+    kill -TERM "$OCTANE_PID" 2>/dev/null
+    kill -TERM "$SSR_PID" 2>/dev/null
+    wait
+    exit 0
+}
+trap cleanup TERM INT
+
+echo "starting inertia ssr..."
+php artisan inertia:start-ssr --no-ansi & SSR_PID=$!
+
+echo "starting frankenphp..."
+php artisan octane:frankenphp --no-ansi & OCTANE_PID=$!
