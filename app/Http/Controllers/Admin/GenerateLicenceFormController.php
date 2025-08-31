a678680b092e05c551b61188a527f892e2bb0eec
Aurélien Devaux <github@aureldvx.net>
2025-08-31 12:38:04 +0200

    feat(licence): prefer open licence form in new tab without download

--- app/Http/Controllers/Admin/GenerateLicenceFormController.php
+++ app/Http/Controllers/Admin/GenerateLicenceFormController.php
@@ -14,11 +14,22 @@ class GenerateLicenceFormController extends Controller
 {
     public function __invoke(Licence $licence)
     {
-        return PDFGenerator::generateInMemory(
+        $filename = Str::snake(sprintf('formulaire_licence%s%s', $licence->person->last_name, $licence->person->first_name));
+
+        $response = PDFGenerator::generateInMemory(
             template: PDFTemplate::FORMULAIRE_LICENCE,
-            filename: Str::snake(sprintf('formulaire_licence%s%s', $licence->person->last_name, $licence->person->first_name)),
+            filename: $filename,
             data: [
                 'licence' => $licence,
+            ],
+        );
+
+        return response(
+            $response->getBody()->getContents(),
+            200,
+            [
+                'Content-Type' => 'application/pdf',
+                'Content-Disposition' => sprintf('inline; filename="%s";', $filename),
             ],
         );
     }
