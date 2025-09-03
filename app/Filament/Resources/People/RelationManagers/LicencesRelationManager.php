bde708dfde27cdc7fbb9362a43bc7ad2475f1faa
Aurélien Devaux <github@aureldvx.net>
2025-09-03 22:21:01 +0200

    feat(licence): automatically set person from licence relation manager

--- app/Filament/Resources/People/RelationManagers/LicencesRelationManager.php
+++ app/Filament/Resources/People/RelationManagers/LicencesRelationManager.php
@@ -3,8 +3,10 @@
 namespace App\Filament\Resources\People\RelationManagers;
 
 use App\Filament\Resources\Licences\LicenceResource;
+use App\Filament\Resources\Licences\Schemas\LicenceForm;
 use Filament\Actions\CreateAction;
 use Filament\Resources\RelationManagers\RelationManager;
+use Filament\Schemas\Schema;
 use Filament\Tables\Table;
 
 class LicencesRelationManager extends RelationManager
@@ -20,11 +22,20 @@ class LicencesRelationManager extends RelationManager
         return false;
     }
 
+    public function form(Schema $schema): Schema
+    {
+        return LicenceForm::configureFromRelationManager($schema);
+    }
+
     public function table(Table $table): Table
     {
         return $table
             ->headerActions([
-                CreateAction::make(),
+                CreateAction::make()->mutateDataUsing(function (array $data): array {
+                    $data['person_id'] = $this->ownerRecord->id;
+
+                    return $data;
+                }),
             ])
             ->filters([]);
     }
